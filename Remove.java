035ff09370ec4725bc6c92694a907d1a98eaf761
NehaVelagapudi <52523435+NehaVelagapudi@users.noreply.github.com>
2020-03-25 19:08:14 +0530

    Add files via upload

---
+++ Remove.java
@@ -0,0 +1,27 @@
+package com.tdd;
+
+public class Remove {
+	public String remove(String s) {
+		String res = s;
+		int stringLength = s.length();
+		
+		if(s.charAt(0)!= 'A'  && s.charAt(1)!= 'A') {
+			res=s;
+		}
+		else if(s.charAt(0) == 'A' && s.charAt(1)!= 'A') {
+			res = s.substring(1,stringLength);
+		}
+		else if(s.charAt(1)== 'A' && s.charAt(0)!= 'A') {
+			res = s.charAt(0) + s.substring(2,stringLength);
+		}
+		else if(s.charAt(0)== 'A'  && s.charAt(1)== 'A') {
+			res=s.substring(2,stringLength);
+		}
+	
+		return res;
+	}
+
+}
+
+
+
